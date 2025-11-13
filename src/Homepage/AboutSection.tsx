c944111ebc67a027950d47984ed48bf585a84942
Sansan <msandartun@gmail.com>
2025-11-13 17:09:54 +0800

    text updatess

--- src/Homepage/AboutSection.tsx
+++ src/Homepage/AboutSection.tsx
@@ -11,22 +11,22 @@ function AboutSection() {
     <div className="md:flex justify-between gap-8 max-w-6xl mx-10 md:mx-auto md:w-auto">
       <div className="flex-1 border-solid	border-2 border-sky-500 max-w-2xl text-[20px] p-4 mb-4 md:mb-0">
         <p className="mb-4">
-          Hello, I’m Sandar <GiCyborgFace className="inline" /> a former
-          accountant who made the leap into web development. In 2024, I decided
-          to step away from the world of balance sheets to follow my true
-          interests <GiCyberEye className="inline" />I wanted a career where I
-          could build, create and solve problems, so I dedicated myself to
-          learning code and developing my own projects.
+          Hello, I’m Sandar <GiCyborgFace className="inline" /> <br />
+          A former accountant who made the leap into web development. In 2024, I
+          decided to step away from the world of balance sheets to follow my
+          true interests <GiCyberEye className="inline" /> <br />I wanted a
+          career where I could build, create and solve problems, so I dedicated
+          myself to learning code and developing my own projects.
         </p>
         <p className="mb-4">
           I began with <TiHtml5 className="inline" /> and{" "}
           <MdCss className="inline" />.
         </p>
         <p>
-          While studying, I also worked with
+          While studying, I also worked with{" "}
           <IoLogoJavascript className="inline" /> to create pages and small
           applications. Once comfortable with JS, I moved on to{" "}
-          <IoLogoReact className="inline" />. As I delved deeper into
+          <IoLogoReact className="inline" />.<br /> As I delved deeper into
           programming, I wanted to go beyond frontend, so I started learning
           Node.js, Express.js and databases.
         </p>
